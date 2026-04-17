7a46f613b50e16cac767fc63137990964216568a
Hasbi Mizan Azzami <devopshasbi@gmail.com>
2026-04-17 10:15:29 +0700

    chore(setup-group): add check for deepflow-ctl installation before download

--- common/scripts/setup-group.sh
+++ common/scripts/setup-group.sh
@@ -9,13 +9,15 @@ DOMAIN_NAME="legacy-host"
 CONFIG_FILE="common/config/agent-group/group-config.yaml"
 DEEPFLOWCTL_VER="${DEEPFLOW_VERSION:-$(grep DEEPFLOW_VERSION .env | cut -d= -f2)}"
 
-# Setup deepflow-ctl
-curl -o /usr/bin/deepflow-ctl \
-"https://deepflow-ce.oss-cn-beijing.aliyuncs.com/bin/ctl/$DEEPFLOWCTL_VER/linux/$(arch \
-| sed 's|x86_64|amd64|' | sed 's|aarch64|arm64|')/deepflow-ctl"
-chmod a+x /usr/bin/deepflow-ctl
+# Setup deepflow-ctl kalau belum ada
+if ! command -v deepflow-ctl &> /dev/null; then
+    echo "deepflow-ctl tidak ditemukan, memulai proses download..."
+    curl -o /usr/bin/deepflow-ctl \
+    "https://deepflow-ce.oss-cn-beijing.aliyuncs.com/bin/ctl/$DEEPFLOWCTL_VER/linux/$(arch \
+    | sed 's|x86_64|amd64|' | sed 's|aarch64|arm64|')/deepflow-ctl"
+    chmod a+x /usr/bin/deepflow-ctl
 
 # Buat agent group dan konfigurasi
-deepflow-ctl agent-group create sekawan
-GROUP_ID=$(deepflow-ctl agent-group list | awk '$1 == "sekawan" {print $2}')
-deepflow-ctl agent-group create $GROUP_ID --config-file $CONFIG_FILE
+deepflow-ctl agent-group create "$GROUP_NAME"
+GROUP_ID=$(deepflow-ctl agent-group list | awk 'NR>1 && $1=="'$GROUP_NAME'" {print $2}')
+deepflow-ctl agent-group create "$GROUP_ID" --config-file "$CONFIG_FILE"
