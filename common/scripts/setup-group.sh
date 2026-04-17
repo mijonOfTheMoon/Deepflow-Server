912bb8fda56ef8ec689c137bc69af7c310dccfe5
Hasbi Mizan Azzami <devopshasbi@gmail.com>
2026-04-17 10:44:46 +0700

    chore(setup-group): enhance installation and configuration messages for deepflow-ctl

--- common/scripts/setup-group.sh
+++ common/scripts/setup-group.sh
@@ -9,7 +9,8 @@ DOMAIN_NAME="legacy-host"
 CONFIG_FILE="common/config/agent-group/group-config.yaml"
 DEEPFLOWCTL_VER="${DEEPFLOW_VERSION:-$(grep DEEPFLOW_VERSION .env | cut -d= -f2)}"
 
-if ! command -v deepflow-ctl &> /dev/null; then
+if ! command -v deepflow-ctl &> /dev/null; then\
+    echo "Installing deepflow-ctl..."
     curl -o /usr/bin/deepflow-ctl \
     "https://deepflow-ce.oss-cn-beijing.aliyuncs.com/bin/ctl/$DEEPFLOWCTL_VER/linux/$(arch \
     | sed 's|x86_64|amd64|' | sed 's|aarch64|arm64|')/deepflow-ctl"
@@ -22,11 +23,14 @@ if [ -n "$GROUP_ID" ]; then
     CONFIG_ID=$(deepflow-ctl agent-group-config list > /dev/null 2>&1 | awk 'NR>1 && $1=="'$GROUP_NAME'" {print $2}')
 
     if [ -n "$CONFIG_ID" ]; then
+        echo "Updating existing group configuration..."
         deepflow-ctl agent-group-config update "$CONFIG_ID" -f "$CONFIG_FILE"
     else
+        echo "Creating new group configuration..."
         deepflow-ctl agent-group-config create "$GROUP_ID" -f "$CONFIG_FILE"     
     fi
 else
+    echo "Creating new agent group and configuration..."
     deepflow-ctl agent-group create "$GROUP_NAME"
     deepflow-ctl agent-group-config create "$GROUP_ID" -f "$CONFIG_FILE"
 fi
