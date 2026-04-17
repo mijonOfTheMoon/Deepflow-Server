79da7ee1da10ebd2844e31226deb77ae90ec0784
Hasbi Mizan Azzami <devopshasbi@gmail.com>
2026-04-17 10:06:28 +0700

    chore: update group configuration and setup script

---
+++ common/config/scripts/setup-group.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e
+
+SERVER_IP="${NODE_IP_FOR_DEEPFLOW:-$(grep NODE_IP_FOR_DEEPFLOW .env | cut -d= -f2)}"
+SERVER_PORT="30417"
+GROUP_NAME="sekawan"
+DOMAIN_NAME="legacy-host"
+CONFIG_FILE="common/config/agent-group/group-config.yaml"
+DEEPFLOWCTL_VER="${DEEPFLOW_VERSION:-$(grep DEEPFLOW_VERSION .env | cut -d= -f2)}"
+
+# Setup deepflow-ctl
+curl -o /usr/bin/deepflow-ctl \
+"https://deepflow-ce.oss-cn-beijing.aliyuncs.com/bin/ctl/$DEEPFLOWCTL_VER/linux/$(arch \
+| sed 's|x86_64|amd64|' | sed 's|aarch64|arm64|')/deepflow-ctl"
+chmod a+x /usr/bin/deepflow-ctl
+
+# Buat agent group dan konfigurasi
+deepflow-ctl agent-group create sekawan
+GROUP_ID=$(deepflow-ctl agent-group list | awk '$1 == "sekawan" {print $2}')
+deepflow-ctl agent-group create $GROUP_ID --config-file $CONFIG_FILE
