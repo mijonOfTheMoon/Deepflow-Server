42f9148429a795d395ac1f6decd2c2063fafacf0
Hasbi Mizan Azzami <devopshasbi@gmail.com>
2026-04-17 10:23:59 +0700

    chore(setup-group): refine deepflowctl version extraction and fix conditional block

--- common/scripts/setup-group.sh
+++ common/scripts/setup-group.sh
@@ -7,7 +7,7 @@ SERVER_PORT="30417"
 GROUP_NAME="sekawan"
 DOMAIN_NAME="legacy-host"
 CONFIG_FILE="common/config/agent-group/group-config.yaml"
-DEEPFLOWCTL_VER="${DEEPFLOW_VERSION:-$(grep DEEPFLOW_VERSION .env | cut -d= -f2)}"
+DEEPFLOWCTL_VER=$(echo "${DEEPFLOW_VERSION:-$(grep DEEPFLOW_VERSION .env | cut -d= -f2)}" | sed 's/^v//')
 
 # Setup deepflow-ctl kalau belum ada
 if ! command -v deepflow-ctl &> /dev/null; then
@@ -16,6 +16,7 @@ if ! command -v deepflow-ctl &> /dev/null; then
     "https://deepflow-ce.oss-cn-beijing.aliyuncs.com/bin/ctl/$DEEPFLOWCTL_VER/linux/$(arch \
     | sed 's|x86_64|amd64|' | sed 's|aarch64|arm64|')/deepflow-ctl"
     chmod a+x /usr/bin/deepflow-ctl
+fi
 
 # Buat agent group dan konfigurasi
 deepflow-ctl agent-group create "$GROUP_NAME"
