d1b93710134246f94abf4bab8e6bf7fcba144e4f
Hasbi Mizan Azzami <devopshasbi@gmail.com>
2026-04-17 10:50:55 +0700

    chore(setup-group): update deepflow-ctl installation path and commands to use local binary

--- common/scripts/setup-group.sh
+++ common/scripts/setup-group.sh
@@ -11,26 +11,26 @@ DEEPFLOWCTL_VER="${DEEPFLOW_VERSION:-$(grep DEEPFLOW_VERSION .env | cut -d= -f2)
 
 if ! command -v deepflow-ctl &> /dev/null; then\
     echo "Installing deepflow-ctl..."
-    curl -o /usr/bin/deepflow-ctl \
+    curl -o common/bin/deepflow-ctl \
     "https://deepflow-ce.oss-cn-beijing.aliyuncs.com/bin/ctl/$DEEPFLOWCTL_VER/linux/$(arch \
     | sed 's|x86_64|amd64|' | sed 's|aarch64|arm64|')/deepflow-ctl"
-    chmod a+x /usr/bin/deepflow-ctl
+    chmod a+x common/bin/deepflow-ctl
 fi
 
-GROUP_ID=$(deepflow-ctl agent-group list | awk 'NR>1 && $1=="'$GROUP_NAME'" {print $2}')
+GROUP_ID=$(common/bin/deepflow-ctl agent-group list | awk 'NR>1 && $1=="'$GROUP_NAME'" {print $2}')
 
 if [ -n "$GROUP_ID" ]; then
-    CONFIG_ID=$(deepflow-ctl agent-group-config list | awk 'NR>1 && $1=="'$GROUP_NAME'" {print $2}')
+    CONFIG_ID=$(common/bin/deepflow-ctl agent-group-config list | awk 'NR>1 && $1=="'$GROUP_NAME'" {print $2}')
 
     if [ -n "$CONFIG_ID" ]; then
         echo "Updating existing group configuration..."
-        deepflow-ctl agent-group-config update "$CONFIG_ID" -f "$CONFIG_FILE"
+        common/bin/deepflow-ctl agent-group-config update "$CONFIG_ID" -f "$CONFIG_FILE"
     else
         echo "Creating new group configuration..."
-        deepflow-ctl agent-group-config create "$GROUP_ID" -f "$CONFIG_FILE"     
+        common/bin/deepflow-ctl agent-group-config create "$GROUP_ID" -f "$CONFIG_FILE"     
     fi
 else
     echo "Creating new agent group and configuration..."
-    deepflow-ctl agent-group create "$GROUP_NAME"
-    deepflow-ctl agent-group-config create "$GROUP_ID" -f "$CONFIG_FILE"
+    common/bin/deepflow-ctl agent-group create "$GROUP_NAME"
+    common/bin/deepflow-ctl agent-group-config create "$GROUP_ID" -f "$CONFIG_FILE"
 fi
